0fd15bab784064aa818f4435dbd6da0749d1ebe2
Phoebe Smith <psjs201@exeter.ac.uk>
2016-11-15 15:02:15 +0000

    Print numbers in increasing order

--- sumnum.c
+++ sumnum.c
@@ -3,21 +3,33 @@ int main()
 {
     int n1, n2, Sum;
     
-    printf("Enter two integers: ");
+    printf("\nEnter two integers: ");
 
     // two integers entered by user are stored using scanf()
     scanf("%d %d", &n1, &n2);
+	
+	// display numbers in increasing order
+	if(n1>n2)
+	{
+		printf("\nThe numbers in increasing order: %d %d", n2, n1);
+	}
+	else
+	{
+		printf("\nThe numbers in increasing order: %d %d",n1, n2);
+	};
 
     // store sum in variable Sum
     Sum = n1 + n2;
 
     // display output     
-    printf("%d + %d = %d\n", n1, n2, Sum);
+    printf("\n\n%d + %d = %d\n", n1, n2, Sum);
     
     // check if the sum is even or odd, 
     // and print result to screen
-    if(Sum % 2 == 0) printf("%d is even\n", Sum);
-    else printf("%d is odd\n", Sum);
+    if(Sum % 2 == 0) printf("\n%d is even\n", Sum);
+    else printf("\n%d is odd\n", Sum);
+	
+	
 
     return 0;
 }
